3b0b5692ec8533135877066d6fd061fee5ec7535
Regolit <Lex800@ya.ru>
2020-03-31 13:59:01 +0300

    Final version

---
+++ index.php
@@ -0,0 +1,212 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+		<link rel="stylesheet" href="css/style.css" crossorigin="anonymous">
+		<link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+		<title>Day Info</title>
+	</head>
+	<body>
+		<nav class="site-header navbar navbar-expand-xl sticky-top py-1">
+			<div class="container d-flex flex-column flex-xl-row justify-content-between align-items-center">
+				<a class="py-2 d-xl-none" id="menu_button"  aria-label="Day Info"><img src="img/menu_ring_3_3.png" alt="">
+					
+				</a>
+				<a class="py-2 d-none d-xl-inline-block" href="" aria-label="Day Info">
+					<img src="img/di_full_white.png" alt="">
+				</a>
+				<a class="menu_item py-2 d-none d-xl-inline-block" href="#general_cart">Общая карта заражения</a>
+				<a class="menu_item py-2 d-none d-xl-inline-block" href="#detail_map">Статистика CSSE</a>
+				<a class="menu_item py-2 d-none d-xl-inline-block" href="#description">Описание</a>
+				<a class="menu_item py-2 d-none d-xl-inline-block" href="#symptoms">Симптомы</a>
+				<a class="menu_item py-2 d-none d-xl-inline-block" href="#prevention">Профилактика</a>
+				<a class="menu_item py-2 d-none d-xl-inline-block" href="#vaccine">Вакцина</a>
+				<a class="menu_item py-2 d-none d-xl-inline-block" data-toggle="modal" data-target="#modalpay" href="#">Поддержать проект</a>
+				
+			</div>
+		</nav>
+		<div class="container">
+			<div class="row"><div class="col text-center wrap_main_rec"><a href="https://www.avito.ru/novosinkovo/zemelnye_uchastki/uchastok_15_sot._izhs_1033040909" target="_blank">Земельный участок вдали от инфекции</a></div></div>
+			<div class="row">
+				<div class="col">
+					
+				</div>
+				<div class="col-xs-12 col-sm-12 col-md-8 frame" id="general_cart">
+					<h3 >Общая карта заражения короновирусм</h3>
+					<iframe id="monitor-virus" src="https://coronavirus-monitor.ru/map" frameBorder="0" height="500" width="1000" style="max-width: 100%; margin-top: -2em"></iframe>
+				</div>
+				<div class="col">
+				</div>
+			</div>
+			<div class="row">
+				<div class="col">
+					
+				</div>
+				<div class="col-xs-12 col-sm-12 col-md-8 frame" id="detail_map">
+					<h3 >Статистика Center for Systems Science and Engineerin</h3>
+					<iframe src="https://www.arcgis.com/apps/opsdashboard/index.html#/85320e2ea5424dfaaa75ae62e5c06e61" frameborder="0" height="500" width="1000" style="max-width: 100%"></iframe>
+				</div>
+				<div class="col">
+					
+				</div>
+			</div>
+			<div class="row">
+				<div class="col">
+					
+				</div>
+				<div class="col-xs-12 col-sm-12 col-md-8" id="description">
+					
+					<h3 >Коронавирус (2019-nCoV)</h3>
+					<p>
+					Коронавирус(2019-nCoV) или уханьский коронавирус - новый штам коронавируса. Впервые был зарегистрирован В Ухане во время вспышки пневнонии.</p>
+					<p>
+					В зону риска попадают люди с ослабленным иммунитетом. Чаще всего пожилые люди и дети.
+					</p>
+					<p>Коронавирус передается воздушно-капельным путем при тесном контакте с носителем вируса, так и контактном способе - через прикосновения и пожатия.</p>
+				</div>
+				<div class="col">
+				</div>
+			</div>
+			<div class="row">
+				<div class="col">
+					
+				</div>
+				<div class="col-xs-12 col-sm-12 col-md-8" id="symptoms">
+					<h3 >Симптомы</h3>
+					<p>Инкубационный период, то есть время, прошедшее от момента заражения до появления первых признаков болезни, составляет от 1 до 14 дней. Первые симптомы при заражении человеком человека могут уже появлятся через 5 дней, а если передача вируса произошла от животного, то инкубационный период продлится дольше. 			Коронавирус может передаваться на этапе инкубационного периода, при этом заболевший не имеет внешних проявлений болезни. Это услугубляет ситуацию с распространением вируса. Так же заболевание может проходить безсимптомно, но в общем случае симптомы такие же, как в случае с ОРВИ:</p>
+					<ul>
+						<li>высокая температура(38°С или выше, озноб)</li>
+						<li>одышка, затруднения при дыхании</li>
+						<li>сухой, непродуктивный кашель</li>
+						<li>боли в области груди</li>
+						<li>головная боль</li>
+						<li>мышечная боль</li>
+						<li>общее недомогание</li>
+						<li>потеря сознания</li>
+					</ul>
+					<p>Реже такие же, как симптомы в случае пневмонии.</p>
+				</div>
+				<div class="col">
+				</div>
+			</div>
+			<div class="row">
+				<div class="col">
+					
+				</div>
+				<div class="col-xs-12 col-sm-12 col-md-8" id="prevention">
+					<h3 >Профилактика</h3>
+					<p>В случае появления симптомов схожих с ОРВИ, в срочном порядке обратиться в медучреждение
+						Старайтесь меньше касаться руками лица, глаз, носа, слизистой
+						В общественных местах носите маску
+						Не касайтесь предметов в общественных местах
+						Чаще проветривать воздух в помещении
+						По возможности избегайте скопления большого количесва людей и животных
+						Избегайте контакта с больными людьми
+						Часто и тщательно мойте руки с мылом, особенно после контактов с животными
+					Дома чаще, чем обычно проводите влажную уборку.</p>
+				</div>
+				<div class="col">
+					
+				</div></div>
+				<div class="row">
+					<div class="col"></div>
+					<div class="col-xs-12 col-sm-12 col-md-8" id="vaccine">
+						
+						<h3 >Вакцина</h3>
+						<p>Вакцины от нового коронавируса в настоящее время нет. Но известно, что китайские ученые совместно с мировым сообществом ведут разработки по ее нахождению. Уже им удалось изолировать вирус, что приближает к получению антивируса.</p>
+					</div>
+					<div class="col"></div>
+				</div>
+				
+			</div>
+		</div>
+		<!-- Modal -->
+		<div class="modal fade" id="modalpay" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+			<div class="modal-dialog" role="document">
+				<div class="modal-content">
+					<div class="modal-header">
+						<h5 class="modal-title" id="exampleModalLabel">Поддержка ресурса</h5>
+						<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+						<span aria-hidden="true">&times;</span>
+						</button>
+					</div>
+					<div class="modal-body">
+						<iframe src="https://money.yandex.ru/quickpay/shop-widget?writer=seller&targets=%D0%9F%D0%BE%D0%B4%D0%B4%D0%B5%D1%80%D0%B6%D0%B8%D1%82%D0%B5%20%D0%BF%D1%80%D0%BE%D0%B5%D0%BA%D1%82%20%D0%BE%D0%B1%D0%B5%D1%81%D0%BF%D0%B5%D1%87%D0%B5%D0%BD%D0%B8%D1%8F%20%D0%BE%D0%B1%D1%8A%D0%B5%D0%BA%D1%82%D0%B8%D0%B2%D0%BD%D0%BE%D0%B9%20%D0%B8%D0%BD%D1%84%D0%BE%D1%80%D0%BC%D0%B0%D1%86%D0%B8%D0%B8&targets-hint=&default-sum=30&button-text=12&hint=&successURL=&quickpay=shop&account=410012193664553" width="100%" height="250" frameborder="0" allowtransparency="true" scrolling="no"></iframe>
+					</div>
+				</div>
+			</div>
+		</div>
+		<a href="#" id="button-up" class="up_btn btn btn-dark">Вверх</a>
+		
+	</div>
+</div>
+</div>
+<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+<div class="container">
+<div class="row"><div class="col text-right">
+	<!-- Yandex.Metrika informer -->
+	<a href="https://metrika.yandex.ru/stat/?id=53073676&amp;from=informer"
+		target="_blank" rel="nofollow"><img src="https://informer.yandex.ru/informer/53073676/3_0_000000FF_000000FF_1_pageviews"
+	style="width:88px; height:31px; border:0;" alt="Яндекс.Метрика" title="Яндекс.Метрика: данные за сегодня (просмотры, визиты и уникальные посетители)" /></a>
+	<!-- /Yandex.Metrika informer -->
+	<!-- Yandex.Metrika counter -->
+	<script type="text/javascript" >
+	(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+	m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+	(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+	ym(53073676, "init", {
+	clickmap:true,
+	trackLinks:true,
+	accurateTrackBounce:true
+	});
+	</script>
+	<noscript><div><img src="https://mc.yandex.ru/watch/53073676" style="position:absolute; right: 0%;" alt="" /></div></noscript>
+
+
+<!-- /Yandex.Metrika counter -->
+</div></div></div>
+<div class="social_net_share"><script src="https://yastatic.net/es5-shims/0.0.2/es5-shims.min.js"></script>
+<script src="https://yastatic.net/share2/share.js"></script>
+<div class="ya-share2" data-services="vkontakte,facebook,odnoklassniki,moimir,twitter,viber,whatsapp,telegram" data-size="s"></div></div>
+
+<!-- /Buton -->
+<script>
+jQuery(document).ready(function() {
+
+var btn = $('#button-up');
+$(window).scroll(function() {
+if ($(window).scrollTop() > 50) {
+btn.addClass('show');
+
+} else {
+btn.removeClass('show');
+}
+});
+});
+</script>
+
+<!-- /Menu -->
+
+
+	
+<script type="text/javascript">
+jQuery(document).ready(function(){
+	
+
+$('#menu_button').click(function(){
+		$('.menu_item').toggleClass( 'd-none d-inline-block');
+
+
+
+
+});
+ });
+</script>
+
+
+
+</body>
+</html>
